e209c4eb48820dceec59f398927b570325116628
rutvik1745 <rutvik1745@gmail.com>
2023-10-25 17:52:12 +0530

    JavaScript

---
+++ 25-10-23/53.js
@@ -0,0 +1,30 @@
+console.log("This is a java Scripts")
+
+var a=1
+function greet(name, greetText="Greetings from JavaScripts"){
+    var a=20
+    console.log(a)
+    let name1 = "Name1"
+    console.log(greetText + " " + name)
+    console.log(name+"is a nice person");
+}
+let name = "rutvik";
+let name2 = "Ravi";
+let name3 = "ram"
+let greetText = "Good Morning"
+greet(name,greetText);
+greet(name2,greetText);
+greet(name3);
+console.log(a)
+
+function sum(a,b,c){
+    let d= a+b+c
+    console.log(d)
+    return d
+}
+
+let r = sum(20,30,40);
+console.log(r)
+
+sum(1,2,3)
+
